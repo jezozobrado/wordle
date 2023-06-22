8dfc9451b69781b585d7f35b6a9a4ba4449ead25
jezozobrado <jezozobrado@gmail.com>
2023-06-22 11:15:08 +0800

    Implement letter checking

--- wordle/src/components/Tile.tsx
+++ wordle/src/components/Tile.tsx
@@ -1,12 +1,28 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Line from "./Line";
+import LETTERS from "../constants/letters";
 
 const Tile = () => {
   const word = "stare";
 
-  const [guesses, setGuesses] = useState(Array(5).fill(""));
+  const [guesses, setGuesses] = useState<string[]>(Array(6).fill(""));
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [row, setRow] = useState(0);
 
-  useEffect(() => {});
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      console.log("before", e.code);
+      if (LETTERS.includes(e.code)) {
+        console.log("after", e.code);
+        setGuesses((prev) => prev.splice(row, 1, prev[row] + e.key));
+      }
+    },
+    [row]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+  });
 
   return (
     <div className="line-container">
