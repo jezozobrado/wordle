3667965a2a7c8f5cba7aad89c26f73b439c376de
jezozobrado <jezozobrado@gmail.com>
2023-06-22 17:48:42 +0800

    Refactor useRef.

--- wordle/src/components/Tile.tsx
+++ wordle/src/components/Tile.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Line from "./Line";
 import LETTERS from "../constants/letters";
 import five from "../utils/five";
@@ -6,26 +6,32 @@ import unFive from "../utils/unFive";
 
 const Tile = () => {
   const [guesses, setGuesses] = useState<string[]>(Array(6).fill("?????"));
-  const row = useRef(0);
+  let { current: row } = useRef(0);
+  let { current: guessesCopy } = useRef(guesses);
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (LETTERS.includes(e.code)) {
       setGuesses((guesses) =>
         guesses.map((guess, i) => {
-          if (i === row.current && unFive(guess).length < 5)
+          if (i === row && unFive(guess).length < 5)
             return five(unFive(guess) + e.key);
           return guess;
         })
       );
+      guessesCopy = guessesCopy.map((guess, i) => {
+        if (i === row && unFive(guess).length < 5)
+          return five(unFive(guess) + e.key);
+        return guess;
+      });
     } else if (e.code === "Backspace" || e.code === "Delete") {
       setGuesses((guesses) =>
         guesses.map((guess, i) => {
-          if (i === row.current) return five(unFive(guess).slice(0, -1));
+          if (i === row) return five(unFive(guess).slice(0, -1));
           return guess;
         })
       );
-    } else if (e.code === "Enter") {
-      row.current = row.current + 1;
+    } else if (e.code === "Enter" && unFive(guessesCopy[row]).length === 5) {
+      row = row + 1;
     }
   };
 
